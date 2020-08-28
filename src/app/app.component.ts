c41403543e83d362e15abeb70fabc9fe3d2b7421
dindasigma <dinda.sgmwty@gmail.com>
2020-08-28 15:56:22 +0700

    add filter functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
+import { Observable } from 'rxjs';
+
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+import { GoogleSheetsDbService } from 'ng-google-sheets-db';
+import { Event, eventAttributesMapping } from './event-list/event-list.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'waktu';
+  events$: Observable<Event[]>;
+
+  constructor(private googleSheetsDbService: GoogleSheetsDbService) {
+    this.getEvents('all')
+  }
+
+  getEvents(which: string) {
+    if (which == 'all') {
+      this.events$ = this.googleSheetsDbService.get<Event>(
+        environment.spreadsheetId, environment.worksheetId, eventAttributesMapping);
+    } else {
+      this.events$ = this.googleSheetsDbService.getActive<Event>(
+        environment.spreadsheetId, environment.worksheetId, eventAttributesMapping, which);
+    }
+  }
 }
--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,24 +1,15 @@
 import { Observable } from 'rxjs';
 
-import { Component, OnInit } from '@angular/core';
-
-import { environment } from '../../environments/environment';
-import { GoogleSheetsDbService } from 'ng-google-sheets-db';
-import { Event, eventAttributesMapping } from './event-list.model';
+import { Component, OnInit, Input } from '@angular/core';
+import { Event } from './event-list.model';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
 })
+
 export class EventListComponent implements OnInit {
-
-  events$: Observable<Event[]>;
-
-  constructor(private googleSheetsDbService: GoogleSheetsDbService) { }
-
+  @Input() events: Observable<Event[]>;
   ngOnInit() {
-    this.events$ = this.googleSheetsDbService.getActive<Event>(
-      environment.spreadsheetId, environment.worksheetId, eventAttributesMapping, 'Active');
   }
-
 }

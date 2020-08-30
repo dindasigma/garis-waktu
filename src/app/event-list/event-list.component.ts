943c2e1ba5332226660cd3992b1858d9b2ca19be
dindasigma <dinda.sgmwty@gmail.com>
2020-08-30 09:34:20 +0700

    fix bug on menu

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Event } from '../app.model';
 
 import { AppService } from '../app.service'
@@ -16,6 +16,7 @@ export class EventListComponent implements OnInit {
   }
 
   ngOnInit() {
+
   }
 
   get events(): Event[] {
